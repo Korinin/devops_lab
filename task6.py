6857e20437f59d75cf5dde9f4dc14ff4c4f947bf
Kirill Shevchenko <minskshop@gmail.com>
2019-06-20 22:47:34 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,46 +1,29 @@
-
 import re
-mystring= input ("input a string :" )
+mystring= input("input a string :" )
 # check for letters and avalibility " = "
 if '=' in mystring and ((re.search("[a-zA-Z]+" ,mystring)) == None): part1, part3 =mystring.split('=')
-else :print ('error')
+else:print('error')
 
-if '+' in mystring : operator = '+'
-elif '-' in mystring : operator = '-'
-elif '*' in mystring : operator = '*'
-elif '/' in mystring : operator = '/'
-else :print ('error')
+if '+' in mystring: operator = '+'
+elif '-' in mystring: operator = '-'
+elif '*' in mystring: operator = '*'
+elif '/' in mystring: operator = '/'
+else:print('error')
 
 part1, part2 =part1.split(operator)
 
-if part1.isdigit() and part2.isdigit() and part3.isdigit() :
-    a = int (part1)
-    b = int (part2)
-    c = int (part3)
+if part1.isdigit() and part2.isdigit() and part3.isdigit():
+    a = int(part1)
+    b = int(part2)
+    c = int(part3)
     if (operator == '+') and (a + b == c):
-        print ('YES')
+        print('YES')
     elif operator == '-' and (a - b == c):
-        print ('YES')
+        print('YES')
     elif operator == '*' and (a * b == c):
-        print ('YES')
+        print('YES')
     elif operator == '/' and (a / b == c):
-        print ('YES')
+        print('YES')
     else:
-        print ('NO')
-else :print ('error')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print('NO')
+else:print('error')
