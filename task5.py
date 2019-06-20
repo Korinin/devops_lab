cdd312af4cbbbbbda6e96612eccc9c275fe20884
Kirill Shevchenko <minskshop@gmail.com>
2019-06-20 22:27:53 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,11 +1,11 @@
-s = int (input ("Input integer value: "))
-print ("Your number is:", s)
-bin1 = bin (s)
+s = int(input("Input: "))
+bin1 = bin(s)
 rList = []
-for i in list (bin1)[2:]:
+for i in list(bin1)[2:]:
     if i == '0':
-        rList.append ('1')
+        rList.append('1')
     else:
-        rList.append ('0')
-r = ''.join (rList)
-print ("Complement number is:", int (r, 2))
+        rList.append('0')
+r = ''.join(rList)
+print("Output:", int(r, 2))
+
