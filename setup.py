030d7a9441d4420e90e84b97cd6710eb1cc07c47
Kirill Shevchenko <minskshop@gmail.com>
2019-06-23 00:56:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "statshot",
-    packages = find_packages(),
-    version = "1.0",
-    author = "Kirill_Shevchenko",
-    author_email = "Kirill_Shevchenko1@epam.com",
-    description = "Phyton.HomeTask.3",
-    include_package_data = True,
-    install_requires = ['psutil'],
-    license = "EPAM"
+    name="statshot",
+    packages=find_packages(),
+    version="1.0",
+    author="Kirill_Shevchenko",
+    author_email="Kirill_Shevchenko1@epam.com",
+    description="Phyton.HomeTask.3",
+    include_package_data=True,
+    install_requires=['psutil'],
+    license="EPAM"
 )
