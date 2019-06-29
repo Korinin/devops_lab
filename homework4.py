8c3a9f570fe62816c35c247060be1b694053bca1
Kirill Shevchenko <minskshop@gmail.com>
2019-06-30 00:28:15 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -2,21 +2,27 @@
 import requests
 import argparse
 
-version = "0.9"
-token = '7c187cd9203eb03cffb2acbf1f4cfee61f8e1532'
+version = "1.0"
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-user', nargs=1, help="write github username", required=True)
-parser.add_argument('-repo', nargs=1, help="write name of repo", required=True)
-parser.add_argument('-field', nargs=1, help="write argument likes id, title, html_url etc... ",
-                    required=True)
+parser.add_argument('-repo', nargs=1, help="write name of repo", required=rue)
+parser.add_argument('-field', nargs=1, help="write argument likes id, title, html_url etc... ", required=True)
+parser.add_argument('-token', nargs=1, help="write name of repo", required=True)
+parser.add_argument('-login', help="write flag for show login", action='store_true')
+parser.add_argument('-ref', help="write flag for show name of pull", action='store_true')
 parser.add_argument('-v', action='version', version='Lastpull 0.9')
+parser.add_argument('--version', action='version', version='Lastpull 0.9')
 
 args = parser.parse_args()
+b = requests.get('https://api.github.com/repos/' + args.user[0] + '/' + args.repo[0] + '/pulls',
+                 auth = ('woraccmsq', args.token[0]))
+
 if args.field[0]:
-
-    b = requests.get('https://api.github.com/repos/' + args.user[0] + '/' + args.repo[0] + '/pulls',
-                     auth=('woraccmsq', token))
     print(b.json()[0][args.field[0]])
-
 else:
-    print('bed arguments')
+    print('bad argument')
+if args.login:
+    print(b.json()[0]['user']['login'])
+if args.ref:
+    print(b.json()[0]['head']['ref'])
